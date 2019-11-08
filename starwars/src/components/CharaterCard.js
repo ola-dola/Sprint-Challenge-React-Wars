41cf43b5911bc38054769caa0e94bd7dc36d4b97
oredola <oredolaolamide@gmail.com>
2019-11-08 11:34:59 +0100

    Styled the cards

--- starwars/src/components/CharaterCard.js
+++ starwars/src/components/CharaterCard.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Card, CardTitle, CardText, CardImg, CardImgOverlay } from 'reactstrap';
 
-function CharacterCard ({data}) {
+function CharacterCard({ data }) {
   return (
     <div>
-      <h3>Name: {data.name}</h3>
-      <p>Height: {data.height}</p>
-      <p>Mass: {data.mass}</p>
-      <p>Hair Color: {data.hair_color}</p>
-      <p>Birth Year: {data.birth_year}</p>
-      <p>Number of films: {data.films.length}</p>
+      <Card inverse>
+        <CardImg width="100%" src="/assets/318x270.svg" alt="Card image cap" />
+        <CardImgOverlay>
+          <CardTitle>Name: {data.name}</CardTitle>
+          <CardText>Height: {data.height}</CardText>
+          <CardText>Mass: {data.mass}</CardText>
+          <CardText>Hair Color: {data.hair_color}</CardText>
+          <CardText>Birth Year: {data.birth_year}</CardText>
+          <CardText>Number of films: {data.films.length}</CardText>
+        </CardImgOverlay>
+      </Card>
     </div>
   )
 }
