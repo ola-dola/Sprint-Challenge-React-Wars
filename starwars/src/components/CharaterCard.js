ffd49d777ccb156c9109ab22578c7f1fc1859acc
oredola <oredolaolamide@gmail.com>
2019-11-08 11:08:19 +0100

    Added extra card details

--- starwars/src/components/CharaterCard.js
+++ starwars/src/components/CharaterCard.js
@@ -7,7 +7,8 @@ function CharacterCard ({data}) {
       <p>Height: {data.height}</p>
       <p>Mass: {data.mass}</p>
       <p>Hair Color: {data.hair_color}</p>
-      {/* <p>Number of films: {data.films.length}</p> */}
+      <p>Birth Year: {data.birth_year}</p>
+      <p>Number of films: {data.films.length}</p>
     </div>
   )
 }
